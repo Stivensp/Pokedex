a4544c87d48304cb2168f05f88ca776da600faf7
Stiven <delleore1@gmail.com>
2024-01-02 11:37:36 -0500

    v6

--- src/components/PokemonPreview.jsx
+++ src/components/PokemonPreview.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "/src/components/styles/PokemonPreview.css"
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 const PokemonPreview = ({pokemonURL}) => {
     const [pokemonInfo, setPokemonInfo] = useState(null)
     useEffect(()=>{
@@ -9,9 +10,39 @@ const PokemonPreview = ({pokemonURL}) => {
         .catch((err) => console.log(err))
       },[]);
     
+      const card = document.getElementById("Cards_");//?Dos variables
+      const CardHover = document.getElementById("Cards_;hover");//?Una variable
+      const Card__header = document.getElementById("Cards_header");//?Una variable
+      const pokemon__name = document.getElementById("pokemon__name");//?Una variable
+      const stat = document.getElementById("stat");//?Una variable
+      
+    const  gradientsByType={
+        grass: {  backgroundColor: "white" },
+        fire: { backgroundColor: "white" },
+        water: { backgroundColor: "white" },
+        electric: { backgroundColor: "white" },
+        normal: { backgroundColor: "white" },
+        poison: { backgroundColor: "red" },
+        ground: { backgroundColor: "white" },
+        fairy: { backgroundColor: "white" },
+        fighting: { backgroundColor: "white" },
+        flying: { backgroundColor: "white" },
+        dragon: { backgroundColor: "white" },
+        ice: { backgroundColor: "white" },
+        ghost: { backgroundColor: "white" },
+        steel: { backgroundColor: "white" },
+        rock: { backgroundColor: "white" },
+        dark: { backgroundColor: "white" },
+        unknown: { backgroundColor: "white" },
+        shadow: { backgroundColor: "white" },
+        bug: { backgroundColor: "white" },
+        psychic: { backgroundColor: "white" },
+      };
+
+
   return (
     
-        <div id='Cards_'>
+        <Link  id='Cards_' to={`/pokedex/${pokemonInfo?.data.id}`}>
         <header id='Card__header'>
             <img id='imgs' src={pokemonInfo?.data.sprites.other["official-artwork"].front_default} alt="" />
         </header>
@@ -26,10 +57,10 @@ const PokemonPreview = ({pokemonURL}) => {
         <div id='Body__type'>
             {
                 pokemonInfo?.data.stats.map((stat)=>(
-                    <li key={stat.stat.name}>{stat.stat.name}</li>
+                    <li key={stat.stat.name}>{stat.stat.name}<br></br> <span id='stat'>{stat.base_stat}</span></li>
                 ))}
         </div>
-        </div>
+        </Link>
    
   );
 };
--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -5,16 +5,46 @@ import PokemonList from '../components/PokemonList'
 import axios from 'axios'
 const Pokedex = () => {
  const [allPokemons, setAllPokemons] = useState([])
-  const handleclick=(e)=>{
+ const [pokemonName, setPokemonName]=useState("")
+ const [types, setTypes]=useState([])
+  const handleSubmit=(e)=>{
     e.preventDefault()
-    console.log(e.target)
+   setPokemonName(e.target.PokemonName.value.toLowerCase())
   }
 
+  const pokemonsByname=allPokemons.filter((pokemon)=>
+  pokemon.name.includes(pokemonName.trim())
+  );
+ console.log(pokemonsByname)
   useEffect(()=>{
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=1302")
     .then((data) => setAllPokemons(data.data.results))
     .catch((err) => console.log(err))
   },[]);
+
+
+    useEffect(()=>{
+      axios.get("https://pokeapi.co/api/v2/type")
+      .then((data) => setTypes(data.data.results))
+      .catch((err) => console.log(err))
+    },[]);
+
+     const handleChangeType=(e)=>{
+       const url= e.target.value;
+       axios
+       .get(e.target.value)
+       .then(({data})=> {
+        if(url.includes("type")){
+        const pokemonFormat=  data.pokemon.map((pokemon)=>pokemon.pokemon);
+        setAllPokemons(pokemonFormat)
+        }else{
+          setAllPokemons(data.results);
+        }
+       
+       })
+       .catch((err) => console.log(err))
+     }
+
 
   const trainerName=useSelector((store)=> store.trainerName.name)
   return (
@@ -34,20 +64,25 @@ const Pokedex = () => {
       </div>
       <div className='third__container'>
 
-      <form className='form' action="">
-        <input className='input' type="text" placeholder='Search' />
-        <button onClick={handleclick} className='btn__search'>Search</button>
+      <form autoComplete='off' onSubmit={handleSubmit} className='form' action="">
+        <input name='PokemonName' className='input' type="text" placeholder='Search' />
+        <button  className='btn__search'>Search</button>
       </form>
       <div id='select'>
-      <select className='select__container' name="" id="">
+
+      <select onChange={handleChangeType}  className='select__container' name="" id="">
         <option value="">All pokemons</option>
+        {types.map((type)=> (
+            <option value={type.url} key={type.name}>{type.name}</option>
+          ))}
+
       </select>
       </div>
     
 
       </div>
     
-      <PokemonList pokemons={allPokemons}/>
+      <PokemonList pokemons={pokemonsByname}/>
  
 
       </main>
